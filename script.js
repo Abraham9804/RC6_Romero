b22ef01b0bb1e1a71e85631e1bb5822f4009381b
Romero <romero9804@hotmail.com>
2022-06-28 19:30:02 -0700

    reto terminado

---
+++ script.js
@@ -0,0 +1,11 @@
+const semana = ['lunes','martes','miercoles','jueves','viernes ']
+
+const finde = []
+
+finde.push('sabado', 'domingo')
+const completa=  semana.concat(finde)
+
+console.log(completa)
+
+
+
